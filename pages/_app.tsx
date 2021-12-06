7ac1447fe9e826d4d56c733bc2622707469024ef
Yarden Refaeli <yard2010@gmail.com>
2021-12-06 17:50:14 +0200

    Add home page with text.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,41 @@
-import "../styles/globals.css";
+import "../styles/reset.css";
+import "../styles/global.css";
+import styled from "@emotion/styled";
 import type { AppProps } from "next/app";
+
+const Wrapper = styled.div({
+  margin: "0 auto",
+  padding: "0 16px",
+});
+
+const HeaderWrapper = styled.div({
+  height: 44,
+  backgroundColor: "#212121",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "stretch",
+});
+
+const Header = styled.div({
+  padding: "0 16px",
+  fontSize: "20px",
+  color: "#F6F6F6",
+  flex: 1,
+});
+
+const Footer = styled.div({});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <header>Header</header>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <HeaderWrapper>
+        <Header>Yarden Refaeli</Header>
+      </HeaderWrapper>
+      <Wrapper>
+        <Component {...pageProps} />
+        <Footer></Footer>
+      </Wrapper>
+    </>
   );
 }
 
