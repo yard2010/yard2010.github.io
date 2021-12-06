c8f136012904503073c6a350b04899aaf30178f1
yard2010 <yard2010@gmail.com>
2021-12-06 17:12:16 +0000

    Deploying to gh-pages from @ yard2010/yard2010.github.io@831682b615fe96fc2ddde7307aed400dfd0e8339 🚀

---
+++ _next/static/F3IvmOdU6Q4Md55kEll6W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8a9b7239efca1e3f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
